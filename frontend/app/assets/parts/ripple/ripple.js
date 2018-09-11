f28232d5f62052362fb0dc4f01e2068ba1c11641
7ime <lopatsin1990@gmail.com>
2018-09-11 20:24:48 +0300

    create block ripple for button and not only

---
+++ frontend/app/assets/parts/ripple/ripple.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+	let ripple = document.querySelectorAll('[data-js="ripple"]');
+
+	ripple.forEach((elem) => {
+		elem.addEventListener('click', function(event) {
+			event.preventDefault();
+
+			let ripple = this.querySelector('.ripple');
+
+			if(ripple) {
+				ripple.remove();
+			}
+
+			let elemWidth = this.offsetWidth;
+			let elemlHeight = this.offsetHeight;
+			let equalSize = null;
+
+			if(elemWidth >= elemlHeight) {
+				equalSize = elemlHeight;
+			}
+			else {
+				equalSize = elemWidth;
+			}
+
+			let x = event.offsetX == 'undefined' ? event.layerX : event.offsetX - equalSize / 2; 
+			let y = event.offsetY == 'undefined' ? event.layerY : event.offsetY - equalSize / 2; 
+
+			let newRipple = document.createElement('span');
+
+			newRipple.className = 'ripple';
+			
+			let s = newRipple.style;
+
+			s.width = equalSize + 'px';
+			s.height = equalSize + 'px';
+
+			s.top = y + 'px';
+			s.left = x + 'px';
+
+			this.appendChild(newRipple);	
+
+		});
+
+	});
+
+});
